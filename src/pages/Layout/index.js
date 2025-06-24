f69703b3a4402311a4ea93af26d3ae25661f458f
cici <2192697382@qq.com>
2025-06-24 17:25:24 +0800

    TabBar切换功能

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,23 +1,64 @@
-import { Button } from "antd-mobile";
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { TabBar } from "antd-mobile";
+import "./index.scss";
 // 异步action
 import { getBillList } from "@/store/modules/billStore";
+// 引入图标
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
 
 const Layout = () => {
+  // 请求账单数据
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBillList());
   }, [dispatch]);
+
+  //   切换菜单,跳转路由
+  const navigate = useNavigate();
+  const swithRoute = (path) => {
+    console.log(path);
+    navigate(path);
+  };
   return (
-    <div>
-      我是layout
-      <Outlet></Outlet>
-      {/* 测试全局生效样式 */}
-      <Button color="primary">测试全局</Button>
-      <div className="puple">
-        <Button color="primary">测试局部</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet></Outlet>
+      </div>
+      <div className="footer">
+        {/* onchange默认传参每一个TabBar的key值 */}
+        <TabBar onChange={swithRoute}>
+          {tabs.map((item) => (
+            <TabBar.Item
+              key={item.key}
+              icon={item.icon}
+              title={item.title}
+            ></TabBar.Item>
+          ))}
+        </TabBar>
       </div>
     </div>
   );
