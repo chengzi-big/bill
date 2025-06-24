fd0ad35a07e96e94c475564251bdafcb50c542df
cici <2192697382@qq.com>
2025-06-24 16:31:10 +0800

    使用RTK管理账单列表

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 // import App from './App';
-import router from '@/router'
-import { RouterProvider } from 'react-router-dom';
+import router from "@/router";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "@/store/index";
 
 // 导入定制主题文件
-import './theme.css'
+import "./theme.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- 
-   <RouterProvider router={router}></RouterProvider>
-  
+  <Provider store={store}>
+    <RouterProvider router={router}></RouterProvider>
+  </Provider>
 );
-
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,18 +1,26 @@
-import { Button } from "antd-mobile"
-import { Outlet } from "react-router-dom"
+import { Button } from "antd-mobile";
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+// 异步action
+import { getBillList } from "@/store/modules/billStore";
 
 const Layout = () => {
-    return (
-        <div>我是layout
-            <Outlet></Outlet>
-            {/* 测试全局生效样式 */}
-            <Button color="primary">测试全局</Button>
-            <div className="puple">
-                <Button color="primary">测试局部</Button>
-            </div>
-        </div>
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBillList());
+  }, [dispatch]);
+  return (
+    <div>
+      我是layout
+      <Outlet></Outlet>
+      {/* 测试全局生效样式 */}
+      <Button color="primary">测试全局</Button>
+      <div className="puple">
+        <Button color="primary">测试局部</Button>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Layout
+export default Layout;
