a187d3b0aa288440de788683b88f0d3195d58571
cici <2192697382@qq.com>
2025-06-26 16:58:05 +0800

    上传项目

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import sum from '@/test'
+
+console.log(sum(1, 3))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+    <App />
+  
+);
+
